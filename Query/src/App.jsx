298f72d8ebf4d0edd598ad3e3888065bbd64eb9f
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-22 14:18:14 +0300

    Mutation

--- Query/src/App.jsx
+++ Query/src/App.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { useQuery } from "@tanstack/react-query";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
@@ -10,23 +10,44 @@ function App() {
 
   /*user her değiştiğinde fonksiyon güncelleniyor*/
   //https://jsonplaceholder.org/posts
-  const { data, isLoading, refetch } = useQuery({
-    queryKey: ['posts'],
-    queryFn: () => fetch('https://jsonplaceholder.org/posts').then(res => res.json()),
-    enabled: false
+  //https://jsonplaceholder.org/users
+  // const { data, isLoading, refetch } = useQuery({
+  //   queryKey: ['posts'],
+  //   queryFn: () => fetch('https://jsonplaceholder.org/posts').then(res => res.json()),
+  //   enabled: false
+  // });
+
+  const [newPost, setNewPost] = useState({
+    title: "foo",
+    body: "bar",
+    userId: 1
   });
 
-  console.log(data, isLoading, "fetchData");
+  const { mutate, isLoading } = useMutation({
+    mutationKey: ['users'], (newPost) => {
+    mutationFn: () => fetch('https://jsonplaceholder.org/users', {
+      method: "POST",
+      body: JSON.stringify(newPost),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8"
+      }
+    }).then(response => response.json())
+  });
 
-  if (isLoading) {
-    return <div>Loading...</div>;
-  }
+  console.log(mutateData, "mutateData");
+
+  // console.log(data, isLoading, "fetchData");
+
+  // if (isLoading) {
+  //   return <div>Loading...</div>;
+  // }
 
   return (
     <div>
-      <button onClick={() => refetch()}>Veri Çek</button>
+      {/* <button onClick={() => refetch()}>Veri Çek</button> */}
+      <button onClick={() => { }}>Veri Çek</button>
       <div>
-        {data &&
+        {/* {data &&
           data.map((dt, i) => {
             return (
               <div key={i}>
@@ -34,7 +55,7 @@ function App() {
                 <p>{dt.body}</p>
               </div>
             );
-          })}
+          })} */}
       </div>
     </div>
   );
