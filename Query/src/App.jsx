b498d06e8def3debc182ac8b819920a2d31703cc
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-22 13:38:36 +0300

    first commit

---
+++ Query/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  /*Redux da state durum yönetimi yapılır */
+  /*Query de endpointlerle alakalı yani verilerle alakalı işlem yapılcaksa da bu kullanılır*/
+
+  return (
+    <div className="App">React</div>
+  )
+}
+
+export default App
