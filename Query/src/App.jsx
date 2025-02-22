4425441ff5bc95d82b08267c4a8b4cdb5d02b691
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-22 13:55:55 +0300

    FetchDataSettings

--- Query/src/App.jsx
+++ Query/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { useQuery } from "@tanstack/react-query";
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
@@ -7,6 +8,16 @@ function App() {
 
   /*Redux da state durum yönetimi yapılır */
   /*Query de endpointlerle alakalı yani verilerle alakalı işlem yapılcaksa da bu kullanılır*/
+
+  /*user her değiştiğinde fonksiyon güncelleniyor*/
+  //https://jsonplaceholder.org/posts
+  const fetchData = useQuery(['posts'], () => {
+    return fetch('https://jsonplaceholder.org/posts').then(response => response.json())
+  })
+
+  console.log(fetchData, 'fetchData');
+
+
 
   return (
     <div className="App">React</div>
