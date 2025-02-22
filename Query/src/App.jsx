b7b9e9f4602811797d22d968a6d513e831207e02
grbzemir <eyetkingurbuz@trakya.edu.tr>
2025-02-22 14:06:18 +0300

    Object

--- Query/src/App.jsx
+++ Query/src/App.jsx
@@ -1,27 +1,43 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from 'react';
 import { useQuery } from "@tanstack/react-query";
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-
   /*Redux da state durum yönetimi yapılır */
   /*Query de endpointlerle alakalı yani verilerle alakalı işlem yapılcaksa da bu kullanılır*/
 
   /*user her değiştiğinde fonksiyon güncelleniyor*/
   //https://jsonplaceholder.org/posts
-  const fetchData = useQuery(['posts'], () => {
-    return fetch('https://jsonplaceholder.org/posts').then(response => response.json())
-  })
+  const { data, isLoading, refetch } = useQuery({
+    queryKey: ['posts'],
+    queryFn: () => fetch('https://jsonplaceholder.org/posts').then(res => res.json()),
+    enabled: false
+  });
 
-  console.log(fetchData, 'fetchData');
+  console.log(data, isLoading, "fetchData");
 
-
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
 
   return (
-    <div className="App">React</div>
-  )
+    <div>
+      <button onClick={() => refetch()}>Veri Çek</button>
+      <div>
+        {data &&
+          data.map((dt, i) => {
+            return (
+              <div key={i}>
+                <h1>{dt.title}</h1>
+                <p>{dt.body}</p>
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
 }
 
-export default App
+export default App;
